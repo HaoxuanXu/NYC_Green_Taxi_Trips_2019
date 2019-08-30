e014d3985a60243a0de283fb7339fa213b1ec9d8
HaoxuanXu <haoxuanxu@uchicago.edu>
2019-08-30 14:24:59 -0500

    modify the get_link file

--- Get_links.py
+++ Get_links.py
@@ -5,9 +5,12 @@ from bs4 import BeautifulSoup
 
 year_range = ['2019']
 url = "https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+}
 
 def get_links(year_range, url, link_list = []):
-    page = requests.get(url)
+    page = requests.get(url, headers = headers)
     print(page.status_code)
 
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -18,14 +21,12 @@ def get_links(year_range, url, link_list = []):
         for tag in a_tags:
             link_list.append(tag['href'])
     link_list = [link for link in link_list if not ('fhv' in link)]
-    yellow_taxi_link = [link for link in link_list if ('yellow' in link)]
     green_taxi_link = [link for link in link_list if ('green' in link)]
-    yellow_taxi_data_links = pd.DataFrame(yellow_taxi_link, columns = ['Taxi Data Links'])
     green_taxi_data_links = pd.DataFrame(green_taxi_link, columns = ['Taxi Data Links'])
-    return yellow_taxi_data_links, green_taxi_data_links
+    return green_taxi_data_links
 
 
-yellow_taxi_data_links, green_taxi_data_links = get_links(year_range, url)
+green_taxi_data_links = get_links(year_range, url)
 
-yellow_taxi_data_links.to_csv('../NYC_Taxi_Trips/data/Links/NYC_Yellow_Taxi_Data_Link.csv', index = False)
+
 green_taxi_data_links.to_csv('../NYC_Taxi_Trips/data/Links/NYC_Green_taxi_Data_Link.csv', index = False)
